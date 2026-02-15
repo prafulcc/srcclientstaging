9ae69ca78478beae00ee81c66ed4adfe64d1926d
prafulcc <praful@tuta.io>
2026-02-15 22:55:27 +0000

    Initial commit of STEMAX project

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			}
+		})
+	}
+};
